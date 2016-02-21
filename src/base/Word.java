12051eaf9d4fe3e711b5658445ef83c3d53803f1
Jorge Lucas <Jorge Lucas>
2016-02-20 23:34:28 -0200

    Adicionado classe bases para implementação dos algoritmos

---
+++ src/base/Word.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Word {
+    private String word;
+    private ArrayList<WordInDoc> quantityByDocs;
+    
+    public Word(){
+        this.quantityByDocs = new ArrayList<>();
+    }
+    
+    public Word(String word){
+        this.word = word;
+        this.quantityByDocs = new ArrayList<>();
+    }
+    
+    /**
+     * @return the word
+     */
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * @param word the word to set
+     */
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    /**
+     * @return the quantityByDocs
+     */
+    public ArrayList<WordInDoc> getQuantityByDocs() {
+        return quantityByDocs;
+    }
+
+    /**
+     * @param wd the quantityByDocs to set
+     */
+    public void setQuantityByDocs(WordInDoc wd) {
+        this.quantityByDocs.add(wd);
+    }
+}
---
+++ src/base/WordInDoc.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base;
+
+/**
+ *
+ * @author Jorge
+ */
+public class WordInDoc {
+    private String idDoc;
+    private int quantity;
+
+    /**
+     * @return the idDoc
+     */
+    public String getIdDoc() {
+        return idDoc;
+    }
+
+    /**
+     * @param idDoc the idDoc to set
+     */
+    public void setIdDoc(String idDoc) {
+        this.idDoc = idDoc;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
